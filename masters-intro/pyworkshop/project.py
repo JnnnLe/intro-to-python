9489ea12a422739a7784ffc72829eb0a363ca704
JnnnLe <jnnnle.sf@gmail.com>
2019-08-16 22:31:22 -0700

    Play with REPL

--- masters-intro/pyworkshop/project.py
+++ masters-intro/pyworkshop/project.py
@@ -1,3 +1,10 @@
-greetings = ['hello', 'world', 'Jennifer']
+greetings = ["hello", "world", "Jennifer"]
 for greeting in greetings: 
-  print(f'{greeting}, World!')
+    print(f"{greeting}, World")
+
+def add_numbers(x, y):
+    return x + y
+
+add_numbers(1, 2)
+
+print(f"The sum of 1 and 2 is {add_numbers(1, 2)}")
